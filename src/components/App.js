931538bd2b00e6c80b9516971a8de7b64131e605
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-04-29 01:21:52 -0400

    commit tree 5a9feba19a01e98fa47ae9f626c9e4e3b265fa56

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [dogImages, setDogImages] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((image) => {
+            setDogImages(image.message);
+            setIsLoaded(true);
+        });
+    }, []);
+
+    if(!isLoaded) return <p>Loading...</p>;
+
+    return <img src={dogImages} alt='A Random Dog'/>
+}
+
+export default App;
